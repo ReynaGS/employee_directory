a66784cd08619429c33857ff93e726dcefa7ac5d
reyna garcia <garcias.reyna@gmail.com>
2020-12-08 19:35:22 -0500

    added sort by name, department and Id

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,12 +4,33 @@ import EmployeeTable from "./EmployeeTable";
 class Wrapper extends React.Component {
     state = {
         employeeList: this.props.employeeList,
-        filterValue: ""
+        filterValue: "",
+        sortType:"Ascending",
+        sortBy: "name",
+        filterBy:"name"
     };
     handleSort = () => {
-        var x = this.state.employeeList.sort((a, b) => a.id - b.id)
+        var x; 
+        if(this.state.sortBy==="id")
+        {
+            x = this.state.employeeList.sort((a, b) => a.id - b.id)
+        }else if(this.state.sortBy==="name"){
+            x = this.state.employeeList.sort((a, b) => a.firstName.localeCompare(b.firstName))
+
+        }else if(this.state.sortBy==="department"){
+            x = this.state.employeeList.sort((a, b) => a.department.localeCompare(b.department))
+        }
+        var newSortType; 
+        if(this.state.sortType==="Ascending"){
+            newSortType = "Descending";
+        }else {
+            x = x.reverse()
+            newSortType = "Ascending";
+        }
+
       
-        this.setState({employeeList:x})
+        this.setState({employeeList:x, sortType:newSortType})
+
     };
     handleFilter = () => {
          
@@ -22,18 +43,47 @@ class Wrapper extends React.Component {
     handleChange = (e) => {
     this.setState({ filterValue: e.target.value });
     };
+    handleSelect = (e) => {
+        // console.log(e.target.value);
+        this.setState({ sortBy: e.target.value });
+    };
+    handleFilterDrop = (e) => {
+        // console.log(e.target.value);
+        this.setState({ filterBy: e.target.value });
+    };
 
 
     render(){
         return(
             <div>
+                <div>
+                    <label>Sort By:</label>
 
-            <button type="button" className="btn btn-secondary " onClick={this.handleSort}>Sort By ID</button>
+                    <select name="employee_sort" id="sortvalues" onChange={this.handleSelect}>
+                        <option selected="selected" value="name">First Name</option>
+                        <option value="department">Department</option>
+                        <option value="id">ID</option>
+                        
+                    </select>
+                </div>
+                <div>
+        <button type="button" className="btn btn-secondary " onClick={this.handleSort}>Sort {this.state.sortType}</button>
 
-                
+                </div>
+
+                <div>
+                    <label>filter By:</label>
+
+                    <select name="employee_sort" id="sortvalues" onChange={this.handleFilterDrop}>
+                        <option selected="selected" value="name">First Name</option>
+                        <option value="department">Department</option>
+                        <option value="id">ID</option>
+
+                    </select>
+                </div>
 
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control" onChange={this.handleChange} placeholder="Filter By Department" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                    <input type="text" className="form-control" onChange={this.handleChange} placeholder={"Filter " + this.state.filterBy} aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         <div className="input-group-append">
                         <button className="btn btn-outline-secondary" onClick={this.handleFilter} type="button" id="button-addon2">Filter</button>
                         </div>
